749791d6959eeb513809c4b95018613fa9bd5513
jains-net <jains-net@github.com>
2016-02-18 00:27:14 +0530

    New Test Project

---
+++ application/models/model_users.php
@@ -0,0 +1,11 @@
+<?php
+
+class model_users extends CI_Model {
+
+	public function getUsers()
+	{
+	   return 'Sahil';
+	}
+}
+
+?>
---
+++ application/views/view_user_info.php
@@ -0,0 +1,13 @@
+<html>
+<title>
+Practise</title>
+
+<body>
+<h1>Hello World</h1>
+</body>
+<?php 
+echo $users;
+
+echo 'Sahil Jain';
+?>
+</html>
